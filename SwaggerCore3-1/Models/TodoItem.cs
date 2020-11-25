03e0f840c4c135d294608d69f618be1e543765d6
chenchang <1814625556@qq.com>
2020-11-25 17:22:11 +0800

    add swagge doc

---
+++ SwaggerCore3-1/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SwaggerCore3_1.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ SwaggerCore3-1/ReadMe.txt
@@ -0,0 +1,12 @@
+﻿1:
+  install-package Swashbuckle.AspNetCore
+
+2:
+  访问路径  http://localhost:5000/swagger
+
+3：setup配置
+  v1与v1需要对上
+
+4：
+  支持Route配置
+  
