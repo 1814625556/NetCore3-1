b48d3987fa8005687fd429949a60b0cc1857cf87
chenchang <1814625556@qq.com>
2020-12-03 14:35:06 +0800

    跨域

---
+++ CrossDomain/MiddleWareCrossDomain/OptionMiddleWare.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossDomain.MiddleWareCrossDomain
+{
+    public class OptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public OptionsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            return BeginInvoke(context);
+        }
+
+        private Task BeginInvoke(HttpContext context)
+        {
+            if (context.Request.Method == "OPTIONS")
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+                context.Response.Headers.Add("Access-Control-Allow-Headers", new[] { "*" });
+                context.Response.Headers.Add("Access-Control-Allow-Methods", new[] { "GET, POST, PUT, DELETE, OPTIONS" });
+                context.Response.Headers.Add("Access-Control-Allow-Credentials", new[] { "true" });
+                context.Response.StatusCode = 200;
+                return context.Response.WriteAsync("OK");
+            }
+            return _next.Invoke(context);
+        }
+    }
+    public static class OptionsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseOptions(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<OptionsMiddleware>();
+        }
+    }
+}
